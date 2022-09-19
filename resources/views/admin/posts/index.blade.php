49a1a685a180551a3695dca7d7c7300d56b2689b
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-19 19:15:17 +0200

    table of posts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>INDEX</h1>
+<div class="container">
+    <table class="table table-dark">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Author</th>
+                <th>Title</th>
+                <th>Post Date</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            @forelse ($post as $post)
+            <tr>
+                <td>{{ $post->id }}</td>
+                <td><a href="{{ route('admin.posts.show', $post->id) }}">{{ $post->author }}</a></td>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->post_date }}</td>
+            </tr>
+                
+            @empty
+                
+            @endforelse
+        </tbody>
+    </table>
+</div>
 @endsection
