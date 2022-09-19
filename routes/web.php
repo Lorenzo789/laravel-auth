2b6a3afe176bae5041efe3674e7e3c7acb720cb7
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-19 19:15:54 +0200

    add route for resource

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,5 @@ Route::middleware('auth')
     ->group( function (){
         Route::get('/', 'HomeController@index')->name('home');
         Route::get('/home', 'PostController@index')->name('index');
+        Route::resource('posts', 'PostController');
     });
